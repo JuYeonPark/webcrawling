64795e07a12023cb3bf957e2bf21618404a5bc27
etilelab <luckperson7@naver.com>
2017-08-12 17:10:54 +0900

    파이썬X웹크롤링 업무자동화 (1) / 간단한 웹크롤링

---
+++ webcrawling01.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+r=requests.get("http://lambutan.dothome.co.kr/")
+c=r.content
+soup=BeautifulSoup(c,"html.parser")
+
+
+all=soup.find("tbody")
+all2=all.find_all("tr",{"class":""})
+
+
+for item in all2:
+    professor=item.find("td",{"class":"professor"}).text
+    lectureName=item.find("td",{"class":"lecture"}).text
+    orther=item.find("td",{"class":"orther"}).text
+    grade = item.find("td", {"class": "grade"}).text
+    evaluation=item.find("td", {"class": "evaluation"}).text
+    print(professor + " / " + lectureName + " / " + orther + " / " + grade + " / " + evaluation)
